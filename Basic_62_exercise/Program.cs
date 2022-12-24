f36dbe540d4eeef76330d0c58a70c6fcc16b7a07
#GorshokPeep <nikitagorshkov58@gmail.com>
2022-12-24 16:43:29 +0300

    Выполнение 1 задачи

--- Basic_62_exercise/Program.cs
+++ Basic_62_exercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Выберите программу от 1 до 62");
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    ExampleOne();
+                break;
+            }
+
+        }
+
+        static void ExampleOne() {
+            Console.WriteLine("Введите ваше имя");
+            var exampleOne = Console.ReadLine();
+            Console.WriteLine("Здраствуйте : " + exampleOne);
         }
     }
 }
