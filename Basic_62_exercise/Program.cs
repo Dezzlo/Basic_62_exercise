2444bb5147dd6a09bdd5d103a471d9d8949c6e48
Никитка <@WIN-P96F2TV7E9I>
2022-12-24 15:50:27 +0300

    Добавьте файлы проекта.

---
+++ Basic_62_exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Basic_62_exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
